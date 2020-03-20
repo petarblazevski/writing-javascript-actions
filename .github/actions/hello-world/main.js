b664fbc9619dfa03c3c99df7f6a47cdbeec505fc
Petar Blazhevski <pblazhevski@mobiquityinc.com>
2020-03-20 08:26:08 +0100

    allow input in all action files

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,1 +1,12 @@
-console.log('hello');
+const core = required("@actions/core");
+
+const firstGreeting = core.getInput("first-greeting");
+const secondGreeting = core.getInput("second-greeting");
+const thirdGreeting = core.getInput("third-greeting");
+
+console.log(`Hello ${firstGreeting}`);
+console.log(`Hello ${secondGreeting}`);
+
+if (thirdGreeting) {
+  console.log(`Hello ${thirdGreeting}`);
+}
